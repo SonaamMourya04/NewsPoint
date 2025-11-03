52c749c58babed8122462f65b5d6201adba4ba8e
SonaamMourya04 <sonammourya7683@gmail.com>
2025-11-03 22:57:44 +0530

    Initial commit

---
+++ src/component/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+    return(
+        <div>
+            <nav>
+                <a>Home</a>
+                <a>About</a>
+                <a>contact</a>
+            </nav>
+            <h1>
+                "Welcome to my Website!"
+            </h1>
+        </div>
+    )
+    
+}
+export default Header;
